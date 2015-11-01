e34a3faa0ead9bfb653e190093b565430562db4a
tera <tera@PeachTree.local>
2015-11-01 12:01:07 +0900

    first commit

---
+++ ios/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RNSwift
+//
+//  Created by tera on 2015/11/01.
+//  Copyright © 2015年 Facebook. All rights reserved.
+//
+
+import Foundation
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        let jsCodeLocation = NSURL(string: "http://localhost:8081/index.ios.bundle?platform=ios&dev=true")
+        let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "RNSwift", initialProperties: nil, launchOptions: launchOptions)
+
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        let rootViewController = UIViewController()
+        rootViewController.view = rootView
+        self.window?.rootViewController = rootViewController
+        self.window?.makeKeyAndVisible()
+
+        return true
+    }
+}
